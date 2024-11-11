2dc18210b944c82ed2cc9d3e71ad2cccdbfe4b1d
jlindermeir <jakob@skasa.de>
2024-11-11 20:33:11 +0100

    actually visit the specified url

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -108,7 +108,7 @@ def load_website(url):
         with sync_playwright() as p:
             browser = p.chromium.launch()
             page = browser.new_page()
-            page.goto('https://www.google.com')
+            page.goto(url)
             page.wait_for_load_state('networkidle')
 
             # Get the visible text of the page
